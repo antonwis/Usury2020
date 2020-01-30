5126d3af7b83f471d5ee36a3eaeb0ce6d99dec65
antonwis <56794229+antonwis@users.noreply.github.com>
2020-01-30 13:49:10 +0200

    class templates

--- src/main/java/fi/metropolia/group8/model/Alias.java
+++ src/main/java/fi/metropolia/group8/model/Alias.java
@@ -1,4 +1,43 @@
 package fi.metropolia.group8.model;
 
 public class Alias {
+
+    private String name;
+    private String description;
+    private int equity = 0;
+
+
+    public Alias(String name, int equity) {
+        this.name = name;
+        this.equity = equity;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getEquity() {
+        return equity;
+    }
+
+    public void setEquity(int equity) {
+        this.equity = equity;
+    }
+
+    public void addMoney(int money) {
+        this.equity += money;
+    }
 }
--- src/main/java/fi/metropolia/group8/model/Victim.java
+++ src/main/java/fi/metropolia/group8/model/Victim.java
@@ -2,4 +2,40 @@ package fi.metropolia.group8.model;
 
 public class Victim {
 
+    private String name;
+    private String address;
+    private String description;
+
+    public Victim(String name, String address, String description) {
+        this.name = name;
+        this.address = address;
+        this.description = description;
+    }
+    public Victim(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
