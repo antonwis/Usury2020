11fd7591ae4d24574c35d1972f5723f82d6a6d4d
antonwis <56794229+antonwis@users.noreply.github.com>
2020-02-09 21:04:21 +0200

    added tests for alias class

---
+++ src/test/java/fi/metropolia/group8/model/AliasTest.java
@@ -0,0 +1,57 @@
+package fi.metropolia.group8.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AliasTest {
+
+    private Alias alias;
+    private final double DELTA = 0.001;
+
+    @BeforeEach
+    void init() {
+        alias = new Alias("Ben Shapiro", "Professional Jew", 9001);
+    }
+
+    @Test
+    void getId() {
+        assertEquals(0, alias.getId(), "ID should be 0");
+    }
+
+    @Test
+    void setName() {
+        alias.setName("George Soros");
+        assertEquals("George Soros", alias.getName(), "Wrong name");
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Ben Shapiro", alias.getName(), "Wrong name");
+    }
+
+    @Test
+    void setDescription() {
+        alias.setDescription("Tiny Hat Man");
+        assertEquals("Tiny Hat Man", alias.getDescription(), "Wrong description");
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals("Professional Jew", alias.getDescription(), "Wrong description");
+    }
+
+
+    @Test
+    void setEquity() {
+        alias.setEquity(5000);
+        assertEquals(5000, alias.getEquity(), "Wrong equity amount");
+    }
+
+    @Test
+    void getEquity() {
+        assertEquals(9001, alias.getEquity(), "Wrong amount.");
+    }
+
+}
--- src/test/java/model/LoanTest.java
+++ src/test/java/model/LoanTest.java
@@ -1,4 +1,5 @@
 package model;
+import fi.metropolia.group8.model.Alias;
 import fi.metropolia.group8.model.Victim;
 import fi.metropolia.group8.model.Loan;
 import fi.metropolia.group8.model.User;
@@ -16,9 +17,9 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class LoanTest {
     private LocalDate taken = LocalDate.now();
     private LocalDate due = LocalDate.of(2020,02,21);
-    private User user = new User("kappa");
+    private Alias alias = new Alias("kappa", "kys", 9000);
     private Victim victim = new Victim("nisti","kappakatu","hintti");
-    private Loan loan = new Loan(user, (float) 20000.0,victim,this.taken,this.due, (float) 1.4);
+    private Loan loan = new Loan(alias, (float) 20000.0,victim,this.taken,this.due, (float) 1.4);
     @BeforeEach
     public void deleteLoan(){
 
