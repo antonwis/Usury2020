d8ab47a4fe6bb4ca1d626cf8739f9f9b074a8778
Hexexe <42712675+Hexexe@users.noreply.github.com>
2020-02-10 11:34:50 +0200

    details WIP

---
+++ src/main/java/fi/metropolia/group8/view/LoanDetailController.java
@@ -0,0 +1,47 @@
+package fi.metropolia.group8.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class LoanDetailController {
+
+    @FXML
+    private ImageView profileImage;
+
+    @FXML
+    private Label issueDate;
+
+    @FXML
+    private Label totalDebt;
+
+    @FXML
+    private Label interest;
+
+    @FXML
+    private Label dueDate;
+
+    @FXML
+    private Label debtRemaining;
+
+    @FXML
+    private Label projectedEarnings;
+
+    @FXML
+    private Label victimName;
+
+    @FXML
+    private Label victimAddress;
+
+    @FXML
+    private Label victimDescription;
+
+    @FXML
+    private Button enforcePayment;
+
+    @FXML
+    private Button modifyLoan;
+
+}
+
