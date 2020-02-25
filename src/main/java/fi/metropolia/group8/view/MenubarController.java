d752f5c0528aacea33e6d36ef2565dc33bd1bbb2
Cochapollo <47157625+Cochapollo@users.noreply.github.com>
2020-02-26 00:36:20 +0200

    menu hommeleit lisää

--- src/main/java/fi/metropolia/group8/view/MenubarController.java
+++ src/main/java/fi/metropolia/group8/view/MenubarController.java
@@ -2,9 +2,21 @@ package fi.metropolia.group8.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class MenubarController {
 
     @FXML
     private MenuBar menuBar;
+    @FXML
+    private MenuItem exitMenuItem;
+
+
+
+    public void exitApp(javafx.event.ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
