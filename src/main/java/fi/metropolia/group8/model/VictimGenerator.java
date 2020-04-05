21956681838709952691046e320c586d47a510ec
antonwis <56794229+antonwis@users.noreply.github.com>
2020-04-05 04:16:02 +0300

    More initial rng autism

--- src/main/java/fi/metropolia/group8/model/VictimGenerator.java
+++ src/main/java/fi/metropolia/group8/model/VictimGenerator.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -14,7 +15,9 @@ public class VictimGenerator {
 
     private List<String> firstNames = null;
     private List<String> lastNames = null;
-    private final ObservableList<Victim> victimList = FXCollections.observableArrayList();
+    private List<String> addresses = null;
+    private List<String> streetType = Arrays.asList(new String[]{" Street", " Drive", " Avenue"});
+    private final ObservableList<Victim> generatedVictimList = FXCollections.observableArrayList();
 
     private static VictimGenerator instance;
 
@@ -36,8 +39,8 @@ public class VictimGenerator {
      * Returns the current randomly generated list of victim objects.
      * @return returns a list of Victim objects
      */
-    public ObservableList<Victim> getVictimList() {
-        return victimList;
+    public ObservableList<Victim> getGeneratedVictimList() {
+        return generatedVictimList;
     }
 
     /**
@@ -61,6 +64,15 @@ public class VictimGenerator {
        } catch (FileNotFoundException ex) {
            ex.printStackTrace();
        }
+
+       try {
+            Scanner scanner = new Scanner(new File("/..../resources/fi/metropolia/group8/data/address.txt"));
+            while(scanner.hasNextLine()) {
+                addresses.add(scanner.nextLine());
+            }
+       } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+       }
    }
 
     /**
@@ -69,11 +81,12 @@ public class VictimGenerator {
      */
    public void generateVictimList(int listSize) {
         readFiles();
-        victimList.clear();
+        generatedVictimList.clear();
         for(int i = 0; i < listSize; i++) {
             String rngName = firstNames.get(new Random().nextInt(firstNames.size())) + " " + lastNames.get(new Random().nextInt(lastNames.size()));
-            Victim victim = new Victim(rngName, "placeholder address", "placeholder description");
-            victimList.addAll(victim);
+            String rngAddress = new Random().nextInt(90 + 1)  + 1 + addresses.get(new Random().nextInt(addresses.size())) + streetType.get(new Random().nextInt(streetType.size()));
+            Victim victim = new Victim(rngName, rngAddress, "placeholder description");
+            generatedVictimList.addAll(victim);
         }
    }
 
