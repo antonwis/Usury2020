6d013cc823d7bb14ae0dc2919585aa2d82057273
Cochapollo <47157625+Cochapollo@users.noreply.github.com>
2020-05-05 21:36:32 +0300

    testejä

---
+++ src/test/java/fi/metropolia/group8/model/EnforceManagerTest.java
@@ -0,0 +1,42 @@
+package fi.metropolia.group8.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnforceManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void threaten() {
+    }
+
+    @Test
+    void extort() {
+    }
+
+    @Test
+    void torture() {
+    }
+
+    @Test
+    void assassinate() {
+    }
+
+    @Test
+    void updateEnforcedActions() {
+    }
+}
---
+++ src/test/java/fi/metropolia/group8/model/LoanCalculatorTest.java
@@ -0,0 +1,66 @@
+package fi.metropolia.group8.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanCalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getLoanTotalSum() {
+    }
+
+    @Test
+    void getLoanOfferTotalSum() {
+    }
+
+    @Test
+    void getInterestProfit() {
+    }
+
+    @Test
+    void getOfferInterestProfit() {
+    }
+
+    @Test
+    void updateEquity() {
+    }
+
+    @Test
+    void completeLoan() {
+    }
+
+    @Test
+    void modifyLoan() {
+    }
+
+    @Test
+    void forfeitLoan() {
+    }
+
+    @Test
+    void repoLoan() {
+    }
+
+    @Test
+    void payHalfLoan() {
+    }
+
+    @Test
+    void modifyLoanValue() {
+    }
+}
