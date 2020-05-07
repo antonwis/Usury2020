9737465057bdc16af987990193e478da760e3a6c
qqmss <47595921+qqmss@users.noreply.github.com>
2020-05-07 12:58:41 +0300

    main class do not extend Application class.

---
+++ src/main/java/fi/metropolia/group8/view/Main/StartPoint.java
@@ -0,0 +1,10 @@
+package fi.metropolia.group8.view.Main;
+
+import fi.metropolia.group8.view.App;
+
+public class StartPoint {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+
+}
